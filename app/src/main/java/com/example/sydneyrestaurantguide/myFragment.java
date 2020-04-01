0e831a8b1c02f382baa8c2e647a5619dd10627cf
Brian <blabbit321@gmail.com>
2020-04-01 15:29:48 +1100

    final

--- app/src/main/java/com/example/sydneyrestaurantguide/myFragment.java
+++ app/src/main/java/com/example/sydneyrestaurantguide/myFragment.java
@@ -16,12 +16,13 @@ import java.text.DecimalFormat;
 
 
 public class myFragment extends Fragment {
-        private ImageView mfood;
+        private ImageView mfood,mfood1, mfood2;
         private TextView mTitle;
         private TextView mRating;
         private TextView mCuisine;
         private TextView mLocation;
         private TextView mDescription;
+        private ImageView mMap;
 
         public myFragment() {
         // Required public and empty constructor
@@ -32,16 +33,19 @@ public class myFragment extends Fragment {
             View v = inflater.inflate(R.layout.fragment, container, false);
             Bundle bundle = getArguments();
             int position = bundle.getInt("POSITION");
-            Restaurants restaurants = Restaurants.getRestaurants().get(position);
+            final Restaurants restaurants = Restaurants.getRestaurants().get(position);
             // Gets arguments from the FragmentManager that was implemented in Detail
             DecimalFormat formatter = new DecimalFormat("#.##");
             String rate = formatter.format(restaurants.getRating()); //used to format double value for string representation
             mfood = v.findViewById(R.id.food);
+            mfood1 = v.findViewById(R.id.food1);
+            mfood2 = v.findViewById(R.id.food2);
             mTitle = v.findViewById(R.id.title);
             mRating = v.findViewById(R.id.rating);
             mCuisine = v.findViewById(R.id.cuisine);
             mLocation = v.findViewById(R.id.location);
             mDescription = v.findViewById(R.id.description);
+            mMap = v.findViewById(R.id.maps);
 // This links all XML elements to all defined variables;
             mTitle.setText(restaurants.getName());
             mRating.setText("Rating: "+rate+"/5");
@@ -49,12 +53,26 @@ public class myFragment extends Fragment {
             mLocation.setText(restaurants.getLocation());
             mDescription.setText(restaurants.getDescription());
             mfood.setImageResource(restaurants.getFood());
+            mfood1.setImageResource(restaurants.getFood1());
+            mfood2.setImageResource(restaurants.getFood2());
 // Sets the value of the XML element
 
+            // set Map image as an onClick for the actual location
+            mMap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openMap(restaurants.getLocation());
+                }
+            });
 
             return v;
 
+        }// Launches Google Maps when clicking on image and enters the location
+        private void openMap(String location) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q="+location));
+            startActivity(intent);
         }
+
 
 
 
